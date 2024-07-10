ca226bb79fa66f54a7005c18eaf311a012891bcb
zoubr99 <Zoubr99@gmail.com>
2024-07-11 00:35:36 +0100

    gpio_cores.h

---
+++ cpp drivers/gpio_cores.h
@@ -0,0 +1,64 @@
+#ifndef _GPIO_H_INCLUDED
+#define _GPIO_H_INCLUDED
+
+// header file for gpio class defenition
+
+// first GpoCore
+
+class GpoCore 
+{
+
+    // register offset
+    enum{
+        DATA_REG = 0
+
+    };
+
+    private:
+
+        uint32_t base_addr;
+        uint32_t wr_data;
+
+    
+    public:
+    // Constructor and Destructure
+    GpoCore(uint32_t core_base_addr);
+    ~GpoCore();
+
+    // function overloading
+    void write(uint32_t data);
+    void write(int bit_value, int bit_pos);
+
+};
+
+
+// now the GpiCore
+
+class GpiCore
+{
+    // register offset
+    enum{
+
+        DATA_REG = 0
+    };
+
+
+
+    private:
+        uint32_t base_addr;
+
+    public:
+        // Constructor and Destructure
+        GpiCore(uint32_t core_base_adde);
+        ~GpiCore();
+
+        //methods
+        uint32_t read(); // read a 32 bit word
+        int read(int bit_pos);
+
+
+
+
+};
+
+#endif
